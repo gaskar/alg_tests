63b01e3f3e492cb4d9efe48f2f6ed69c37f14cfd
gaskar <gaskar18@gmail.com>
2013-08-04 22:57:20 +0400

    modifications on Node class

--- dstree.js
+++ dstree.js
@@ -22,14 +22,23 @@ Node.prototype.getRightNode = function(){
 	return this._rightNode;
 };
 
-Node.prototype.setLeftNode = function(key, value){
+Node.prototype.addLeftNode = function(key, value){
 	this._leftNode = new Node(key, value);
+	return this._leftNode;
 };
 
-Node.prototype.setRightNode = function(key, value){
+Node.prototype.addRightNode = function(key, value){
 	this._rightNode = new Node(key, value);
+	return this._rightNode;
 }
 
+function Tree(root){
+	this._root = root || new Node(); 
+}
+
+Tree.prototype.getRoot = function(){
+	return this._root;
+};
 
 
 var rootNode = new Node(1, 2);
