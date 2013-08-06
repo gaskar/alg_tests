6c6c31464e9eee9606bad614e5b6e42bad2c2891
gaskar <gaskar18@gmail.com>
2013-08-07 01:57:00 +0400

    Tree class, depth search implementation

--- dstree.js
+++ dstree.js
@@ -24,13 +24,33 @@ Node.prototype.getRightNode = function(){
 
 Node.prototype.addLeftNode = function(key, value){
 	this._leftNode = new Node(key, value);
-	return this._leftNode;
+	return this.getLeftNode();
 };
 
 Node.prototype.addRightNode = function(key, value){
 	this._rightNode = new Node(key, value);
-	return this._rightNode;
-}
+	return this.getRightNode();
+};
+
+Node.prototype.getChildNodes = function(node){
+
+};
+
+Node.prototype.getLeftChildLeftNode = function(){
+	return this.getLeftNode().getLeftNode();
+};
+
+Node.prototype.getLeftChildRightNode = function(){
+	return this.getLeftNode().getRightNode();
+};
+
+Node.prototype.getRightChildLeftNode = function(){
+	return this.getRightNode().getLeftNode();
+};
+
+Node.prototype.getRightChildRightNode = function(){
+	return this.getRightNode().getRightNode();
+};
 
 function Tree(root){
 	this._root = root || new Node(); 
@@ -40,5 +60,29 @@ Tree.prototype.getRoot = function(){
 	return this._root;
 };
 
+Tree.prototype.getNodes = function(node, position){
+	
+	if(node == null)
+		return;
 
-var rootNode = new Node(1, 2);
+	var prefix = '';
+	for(var i = 0; i < position; i++)
+		prefix += '--';
+
+	console.log(prefix + '->' + node.getValue() + '(' + node.getKey() + ')');
+
+	position++;
+	this.getNodes(node.getLeftNode(), position);
+	this.getNodes(node.getRightNode(), position);
+};
+
+Tree.prototype.nodes = {};
+
+var tree = new Tree(new Node(1, 2));
+
+var leftNode = tree.getRoot().addLeftNode(2, 4);
+var rightNode = tree.getRoot().addRightNode(3, 6);
+
+leftNode.addLeftNode(4, 20);
+
+tree.getNodes(tree.getRoot(), 0);
