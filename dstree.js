8f26c420d44d79b45434de062e65171759a79847
gaskar <gaskar18@gmail.com>
2013-08-09 01:14:36 +0400

    finding right path implementation

--- dstree.js
+++ dstree.js
@@ -1,6 +1,8 @@
 function Node(key, value){
 	this._key = key || null;
 	this._value = value || null;
+	this._sum = 0;
+	this._calculated = false;
 
 	this._leftNode = null;
 	this._rightNode = null;
@@ -36,6 +38,19 @@ Node.prototype.getChildNodes = function(node){
 
 };
 
+Node.prototype.addToSum = function(value){
+	this._sum += value;
+	this._calculated = true;
+};
+
+Node.prototype.getSum = function(){
+	return this._sum;	
+};
+
+Node.prototype.isCalculated = function(){
+	return this._calculated;
+}
+
 Node.prototype.getLeftChildLeftNode = function(){
 	return this.getLeftNode().getLeftNode();
 };
@@ -53,11 +68,24 @@ Node.prototype.getRightChildRightNode = function(){
 };
 
 function Tree(root){
-	this._root = root || new Node(); 
+	this._root = root || new Node(0, 0); 
 }
 
 Tree.prototype.getRoot = function(){
 	return this._root;
+};
+
+Tree.prototype.addSumToNodes = function(node){
+	
+	if(node == null)
+		return 0;
+
+	
+	node.addToSum(this.addSumToNodes(node.getLeftNode()));
+    node.addToSum(this.addSumToNodes(node.getRightNode()));
+
+
+	return node.getValue() + node.getSum();
 };
 
 Tree.prototype.getNodes = function(node, position){
@@ -69,11 +97,27 @@ Tree.prototype.getNodes = function(node, position){
 	for(var i = 0; i < position; i++)
 		prefix += '--';
 
-	console.log(prefix + '->' + node.getValue() + '(' + node.getKey() + ')');
+	console.log(prefix + '->' + node.getValue() + '(' + node.getKey() + ')' + '[' + node.getSum() + ']');
 
 	position++;
 	this.getNodes(node.getLeftNode(), position);
 	this.getNodes(node.getRightNode(), position);
+
+};
+
+Tree.prototype.getRightPath = function(node, position){
+	
+	if(node == null)
+		return;
+
+	var prefix = '';
+	for(var i = 0; i < position; i++)
+		prefix += '--';
+
+	console.log(prefix + '->' + node.getValue() + '(' + node.getKey() + ')' + '[' + node.getSum() + ']');
+
+	this.getRightPath(node.getLeftNode().getSum() >= node.getRightNode().getSum() ? node.getLeftNode() : node.getRightNode(), ++position);
+
 };
 
 Tree.prototype.nodes = {};
@@ -81,10 +125,21 @@ Tree.prototype.nodes = {};
 var tree = new Tree(new Node(1, 2));
 
 var leftNode = tree.getRoot().addLeftNode(2, 4);
-var rightNode = tree.getRoot().addRightNode(3, 6);
+var rightNode = tree.getRoot().addRightNode(3, 60);
 
-leftNode
-	.addLeftNode(4, 20)
-	;
+var secLeftNode = leftNode.addLeftNode(4, 120);
+leftNode.addRightNode(5, 60);
+
+secLeftNode.addLeftNode(8, 1000);
+
+rightNode.addLeftNode(6, 300);
+rightNode.addRightNode(7, 500);
+
 
 tree.getNodes(tree.getRoot(), 0);
+tree.addSumToNodes(tree.getRoot(), 0);
+console.log("_calculated");
+tree.getNodes(tree.getRoot(), 0);
+console.log("right path");
+
+tree.getRightPath(tree.getRoot(), 0);
